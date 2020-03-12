854ca5b51aea58d39610d8269229b30dccbc3739
Ben Auffarth <ben.auffarth@wcn.co.uk>
2020-03-12 17:22:58 +0000

    clean up; add openMP template for parallelization between trees

--- toC.py
+++ toC.py
@@ -1,3 +1,17 @@
+import re
+
+
+OPENMP_TEMPLATE = """
+#pragma omp parallel
+{
+  #pragma omp for reduction(+:mean)
+  for (int i=0; i<aSize; i++) {
+     mean = mean + {};
+  }
+}
+"""
+
+
 def tree2fun(tree, feature_names, fun_name='tree_fun'):
     '''Create tree function header + code
     '''
@@ -9,33 +23,44 @@ def tree2fun(tree, feature_names, fun_name='tree_fun'):
     get_code(tree, feature_names)
     print('}')
 
+
 def get_code(tree, feature_names):
     '''Create C code from decision tree.
-    After https://stackoverflow.com/questions/20224526/how-to-extract-the-decision-rules-from-scikit-learn-decision-treefter
+
+    Based on:
+    https://stackoverflow.com/questions/20224526/
+    how-to-extract-the-decision-rules-from-scikit-learn-decision-treefter
     '''
     def print_values(val):
         for i, v in enumerate(val[0]):
-            print('confidences[{}] = {};'.format(i, v))  
+            print('confidences[{}] = {};'.format(i, v))
         print('return;')
-    left      = tree.tree_.children_left
-    right     = tree.tree_.children_right
+    left = tree.tree_.children_left
+    right = tree.tree_.children_right
     threshold = tree.tree_.threshold
-    features  = [feature_names[i] for i in tree.tree_.feature]
+    features = [feature_names[i] for i in tree.tree_.feature]
     value = tree.tree_.value
 
     def recurse(left, right, threshold, features, node):
-            if (threshold[node] != -2):
-                print("if ( " + features[node] + " <= " + str(threshold[node]) + " ) {")
-                if left[node] != -1:
-                    recurse (left, right, threshold, features,left[node])
-                print("} else {")
-                if right[node] != -1:
-                    recurse (left, right, threshold, features,right[node])
-                print("}")
-            else:
-                print_values(value[node])
+        if threshold[node] != -2:
+            print(
+                "if ( " +
+                features[node] +
+                " <= " +
+                str(threshold[node])
+                + " ) {"
+            )
+            if left[node] != -1:
+                recurse(left, right, threshold, features, left[node])
+            print("} else {")
+            if right[node] != -1:
+                recurse(left, right, threshold, features, right[node])
+            print("}")
+        else:
+            print_values(value[node])
 
     recurse(left, right, threshold, features, 0)
+
 
 def slugify(feature_name):
     '''get usable names from feature names, i.e. no spaces,
@@ -43,7 +68,9 @@ def slugify(feature_name):
     Example:
     >> list(data.target_names)
     ['setosa', 'versicolor', 'virginica']
-    >> feature_names = [slugify(feature_name) for feature_name in data.feature_names]
+    >> feature_names = [
+        slugify(feature_name) for feature_name in data.feature_names
+    ]
     ['sepal_length__cm_',
      'sepal_width__cm_',
      'petal_length__cm_',
