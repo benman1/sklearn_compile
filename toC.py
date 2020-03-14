fd1ffc9b9f8cb848bc34f45b4fcf6b01cc4c8af6
benman1 <auffarth@gmail.com>
2020-03-14 09:34:06 +0000

    write to buffer instead stdout; integrate with cffi

--- toC.py
+++ toC.py
@@ -1,4 +1,6 @@
 import re
+import io
+from contextlib import redirect_stdout
 
 
 OPENMP_TEMPLATE = """
@@ -13,19 +15,22 @@ OPENMP_TEMPLATE = """
 
 
 def random_forest(clf, feature_names):
-    print('float (*fcnPtr)({});'.format(', '.join(['float'] * len(feature_names))));
+    with io.StringIO() as buf, redirect_stdout(buf):
+        print('float (*fcnPtr)({});'.format(', '.join(['float'] * len(feature_names))));
+    return buf.getvalue()
 
 def tree2fun(tree, feature_names, fun_name='tree_fun'):
     '''Create tree function header + code
     '''
-    print('void {}('.format(fun_name), end='')
-    for feature in feature_names:
-        print('float {}, '.format(feature), end='')
-    print('float *confidences', end='')
-    print(') {')
-    get_code(tree, feature_names)
-    print('}')
-
+    with io.StringIO() as buf, redirect_stdout(buf):
+        print('void {}('.format(fun_name), end='')
+        for feature in feature_names:
+            print('float {}, '.format(feature), end='')
+        print('float *confidences', end='')
+        print(') {')
+        print(get_code(tree, feature_names))
+        print('}')
+        return buf.getvalue()
 
 def get_code(tree, feature_names):
     '''Create C code from decision tree.
@@ -38,11 +43,6 @@ def get_code(tree, feature_names):
         for i, v in enumerate(val[0]):
             print('confidences[{}] = {};'.format(i, v))
         print('return;')
-    left = tree.tree_.children_left
-    right = tree.tree_.children_right
-    threshold = tree.tree_.threshold
-    features = [feature_names[i] for i in tree.tree_.feature]
-    value = tree.tree_.value
 
     def recurse(left, right, threshold, features, node):
         if threshold[node] != -2:
@@ -62,8 +62,15 @@ def get_code(tree, feature_names):
         else:
             print_values(value[node])
 
-    recurse(left, right, threshold, features, 0)
+    with io.StringIO() as buf, redirect_stdout(buf):
+        left = tree.tree_.children_left
+        right = tree.tree_.children_right
+        threshold = tree.tree_.threshold
+        features = [feature_names[i] for i in tree.tree_.feature]
+        value = tree.tree_.value
 
+        recurse(left, right, threshold, features, 0)
+        return buf.getvalue()
 
 def slugify(feature_name):
     '''get usable names from feature names, i.e. no spaces,
